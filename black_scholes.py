70eaf66083f59e6339dc407ae3f5b02a5b4854d4
Lekton <869944+takato3000@users.noreply.github.com>
2026-01-06 17:12:53 +0000

    update tests

--- black_scholes.py
+++ black_scholes.py
@@ -5,15 +5,26 @@ import streamlit as st
 from pandas import DataFrame
 import black_scholes_functions as bs
 
-def calculate(S: Union[int, float, np.ndarray], K: Union[int, float, np.ndarray], T: Union[float, np.ndarray], r: Union[float, np.ndarray], sigma: Union[float, np.ndarray]) -> DataFrame: 
+
+def calculate(
+    S: Union[int, float, np.ndarray],
+    K: Union[int, float, np.ndarray],
+    T: Union[float, np.ndarray],
+    r: Union[float, np.ndarray],
+    sigma: Union[float, np.ndarray],
+) -> DataFrame:
     if isinstance(S, np.ndarray):
         result = calculate_array(S, K, T, r, sigma)
     else:
         result = calculate_single_point(S, K, T, r, sigma)
     return result
 
-def calculate_array(S, K, T, r, sigma):
+
+def calculate_array(
+    S: np.ndarray, K: np.ndarray, T: np.ndarray, r: np.ndarray, sigma: np.ndarray
+) -> DataFrame:
     params = bs.BlackScholesParams(S, K, T, r, sigma)
+    # columns = ["strike", "call_price", "call_delta", "put_price", "put_delta", "gamma", "theta", "vega"]
     result = DataFrame()
     result["strike"] = K
     result["call_price"] = params.call_price()
@@ -25,8 +36,27 @@ def calculate_array(S, K, T, r, sigma):
     result["vega"] = params.call_vega()
     return result
 
-def calculate_single_point(S, K, T, r, sigma):
-    result = DataFrame(data=np.zeros((1, 8)), columns=["strike", "call_price", "call_delta", "put_price", "put_delta", "gamma", "theta", "vega"])
+
+def calculate_single_point(
+    S: Union[int, float],
+    K: Union[int, float],
+    T: Union[int, float],
+    r: Union[int, float],
+    sigma: Union[int, float],
+) -> DataFrame:
+    result = DataFrame(
+        data=np.zeros((1, 8)),
+        columns=[
+            "strike",
+            "call_price",
+            "call_delta",
+            "put_price",
+            "put_delta",
+            "gamma",
+            "theta",
+            "vega",
+        ],
+    )
     result.loc[0, "strike"] = K
     params = bs.BlackScholesParams(S, K, T, r, sigma)
     result.loc[0, "call_price"] = params.call_price()
@@ -38,15 +68,16 @@ def calculate_single_point(S, K, T, r, sigma):
     result.loc[0, "vega"] = params.call_vega()
     return result
 
-st.set_page_config(page_title="Black Scholes Calculator", layout="wide")
-with st.form(key="parameters"):
-    S = st.number_input("Underlying Price(SP)", 0)
-    K = st.number_input("Strike Price(ST)")
-    T = st.number_input("Time to Expiration in Years(t)")
-    sigma = st.number_input("Volatility(v)")
-    r = st.number_input("Risk-Free Interest Rate(r)")
-    submitted = st.form_submit_button("Calculate")
-    if submitted:
-        result = calculate(S, K, T, r, sigma)
-        st.write(f"Underlying Price: {S}")
-        st.dataframe(result)
+if __name__ == "__main__":
+    st.set_page_config(page_title="Black Scholes Calculator", layout="wide")
+    with st.form(key="parameters"):
+        S = st.number_input("Underlying Price(SP)", 0)
+        K = st.number_input("Strike Price(ST)")
+        T = st.number_input("Time to Expiration in Years(t)")
+        sigma = st.number_input("Volatility(v)")
+        r = st.number_input("Risk-Free Interest Rate(r)")
+        submitted = st.form_submit_button("Calculate")
+        if submitted:
+            result = calculate(S, K, T, r, sigma)
+            st.write(f"Underlying Price: {S}")
+            st.dataframe(result)
--- test_black_scholes.py
+++ test_black_scholes.py
@@ -1,38 +1,50 @@
 import numpy as np
+import unittest
 import pytest
-import pandas as pd
 
-from black_scholes_functions import *
-from black_scholes import *
-
-@pytest.fixture
-def data():
-    return dict({
-        "S": np.array([300, 300]),
-        "K": np.array([250, 350]),
-        "T": np.array([1.0, 1.0]),
-        "r": np.array([0.03, 0.03]),
-        "sigma": np.array([0.15, 0.15]),
-    })
-
-def test_d1(data):
-    assert np.allclose(d1(**data), [1.490, -0.753], atol=1e-3)
+from black_scholes_functions import (
+    BlackScholesParams as BlackScholesParams
+)
+from black_scholes import (
+    calculate_array as calculate_array,
+    calculate_single_point as calculate_single_point,
+)
 
 
-def test_call_price(data):
-    assert np.allclose(call_price(**data), [58.820, 5.471], atol=1e-3)
+class TestBlackScholesFunctions(unittest.TestCase):
+    def setUp(self):
+        self.BSP = BlackScholesParams(np.array([300, 300]), np.array([250, 350]), np.array([1.0, 1.0]), np.array([0.03, 0.03]), np.array([0.15, 0.15]))
+        self.data = dict(
+            {
+                "S": np.array([300, 300]),
+                "K": np.array([250, 350]),
+                "T": np.array([1.0, 1.0]),
+                "r": np.array([0.03, 0.03]),
+                "sigma": np.array([0.15, 0.15]),
+            }
+        )
+        return super().setUp()
+    
+    def test_d1(self):
+        assert np.allclose(self.BSP.d1(), [1.490, -0.753], atol=1e-3)
+    
+    def test_call_price(self):
+        assert np.allclose(self.BSP.call_price(), [58.820, 5.471], atol=1e-3)
+    
+    def test_call_delta(self):
+        assert np.allclose(self.BSP.call_delta(), [0.932, 0.226], atol=1e-3)
+    
+    def test_bs_in_dataframe(self):
+        result = calculate_array(**self.data)
+        assert np.allclose(result["call_delta"].to_numpy(), [0.932, 0.226], atol=1e-3)
+        assert np.allclose(result["call_price"].to_numpy(), [58.820, 5.471], atol=1e-3)
 
 
-def test_call_delta(data):
-    ans = call_delta(**data)
-    assert np.allclose(ans, [0.932, 0.226], atol=1e-3)
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestBlackScholesFunctions))
+    return suite
 
-def test_bs_class(data):
-    bs = BlackScholesParams(**data)
-    assert bs.__dict__ == data
-
-def test_bs_in_dataframe(data):
-    result = calculate_array(**data)
-    assert np.allclose(result["call_delta"].to_numpy(), [0.932, 0.226], atol=1e-3)
-    assert np.allclose(result["call_price"].to_numpy(), [58.820, 5.471], atol=1e-3)
-
+if __name__ == '__main__':
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
